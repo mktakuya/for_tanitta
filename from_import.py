200a85ffbc0124a5f48fc79f4826ef652c0d8c54
mktakuya <takuya.mk96@gmail.com>
2014-04-22 20:53:23 +0900

    love lovely commit

---
+++ from_import.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding:utf-8
+from package.module import MyClass
+
+my_instance = MyClass()
+my_instance.my_method()
+
---
+++ package/module.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+
+class MyClass(object):
+    def my_method(self):
+        print(u"This is my method!")
+
